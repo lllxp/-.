40ea17c5332583dcd6df4097173dfcf0b19235ed
lllxp <1261543653@qq.com>
2019-04-09 23:55:18 +0800

    first commit

---
+++ MyProject_01/src/Address/FlightInterface.java
@@ -0,0 +1,8 @@
+package Address;
+
+public interface FlightInterface {
+	public int[ ] reserve(String names);       //预订航班座位
+	public boolean cancel(int bokingNumber);  //取消预订座位
+	public Passenger[] getPsaaengerList();   //返回旅客列表
+
+}

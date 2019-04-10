f9e0f2513c6d50e1cba2f6dcea8c88174953071d
lllxp <1261543653@qq.com>
2019-04-10 22:10:21 +0800

    first commit

---
+++ MyProject_01/src/Address/PassengerInterface.java
@@ -0,0 +1,8 @@
+package Address;
+
+public interface PassengerInterface {
+	public String getName();        //获取旅客姓名
+	public int getBookingNumber();  //获取预定号
+	public int getRow();           //获取座位排数
+	public int getSeatPosition();   //获取座位号
+}

95949192ed3de338c154fdb2ee9fe2278947bc8a
lllxp <1261543653@qq.com>
2019-05-28 22:41:05 +0800

    first commit

--- MyProject_01/src/新特性/NewCharacter.java
+++ MyProject_01/src/新特性/NewCharacter.java
@@ -1,5 +1,7 @@
 package 新特性;
 
+import java.util.function.Consumer;
+
 class Apple {
 
 	String color;
@@ -17,3 +19,34 @@ class Apple {
 	}
 
 }
+public class NewCharacter {
+
+	public static void ConsumerApple(Apple[] apps, Consumer<Apple> c) {
+		for (Apple app : apps) {
+			c.accept(app);
+		}
+	}
+	public static void main(String[] args) {
+		Apple app1 = new Apple("红", 1);
+		Apple app2 = new Apple("黄", 2);
+		Apple app3 = new Apple("绿", 3);
+
+		Apple[] Apples = { app1, app2, app3 };
+		System.out.println("匿名内部类形式:");
+		ConsumerApple(Apples, new Consumer<Apple>() {
+			@Override
+			public void accept(Apple t) {
+				System.out.println(t);
+			}
+		});	
+		System.out.println();	
+		//lambda表达式
+		System.out.println("lambda表达式:");
+		ConsumerApple(Apples, apple ->{
+			System.out.println(apple.toString());
+		});
+
+	}
+
+}
+

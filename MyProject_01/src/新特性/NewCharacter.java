6369bc0eb2520e58ab6fa99de0f2a06ea91ea4b4
lllxp <1261543653@qq.com>
2019-05-28 22:28:10 +0800

    first commit

--- MyProject_01/src/新特性/NewCharacter.java
+++ MyProject_01/src/新特性/NewCharacter.java
@@ -1,5 +1,19 @@
 package ÐÂÌØÐÔ;
 
-public class NewCharacter {
+class Apple {
+
+	String color;
+	int weight;
+
+	public Apple(String color, int weight) {
+		super();
+		this.color = color;
+		this.weight = weight;
+	}
+
+	@Override
+	public String toString() {
+		return "Apple [color=" + color + ", weight=" + weight + "]";
+	}
 
 }

ebb4814c5ebad77c2968a21a030d75c68a0dc156
lllxp <1261543653@qq.com>
2019-05-28 21:15:10 +0800

    first commit

--- MyProject_01/src/新特性/Apple.java
+++ MyProject_01/src/新特性/Apple.java
@@ -1,5 +1,15 @@
 package ÐÂÌØÐÔ;
-
-public class Apple {
-
+import java.util.function.Consumer;
+class Apple{
+	String color;
+	int weight;
+	public Apple(String color, int weight) {
+		super();
+		this.color = color;
+		this.weight = weight;
+	}
+	@Override
+	public String toString() {
+		return "Apple [color=" + color + ", weight=" + weight + "]";
+	}
 }
